1ee3b108aa79c0753bc8e8da58ec2953ddb43059
Florent Espanet <florent@lumapps.com>
2017-03-14 13:11:48 +0100

    Add singleread mode

--- play.py
+++ play.py
@@ -1,9 +1,12 @@
+import sys
+
 from audio import Audio
+from singleread import singleread
 
 
 def main():
     with Audio() as audio:
-        for x in raw_input():
+        for x in singleread() if "-u" in sys.argv else raw_input():
             audio.play((ord(x) - ord('A') + 1) * 50, 0.3)
 
 
