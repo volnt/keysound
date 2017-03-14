ae1cb6963a3d8bb11f8b124db23b71d40f52ce74
Florent Espanet <florent@lumapps.com>
2017-03-14 14:11:49 +0100

    Shorter counds

--- play.py
+++ play.py
@@ -7,7 +7,7 @@ from singleread import singleread
 def main():
     with Audio() as audio:
         for x in singleread() if "-u" in sys.argv else raw_input():
-            audio.play((ord(x) - ord('A') + 1) * 50, 0.3)
+            audio.play((ord(x) - ord('A') + 1) * 50, 0.1)
 
 
 if __name__ == "__main__":
